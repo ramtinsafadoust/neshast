db1591d6b6599a2f20fe21c5b6db3286468bf204
Ramtin Safadoust <ramtin.safadoust@gmail.com>
2020-12-01 14:19:49 +0330

    Nice Button

--- app/src/main/java/com/oracto/jitsi/MainActivity.java
+++ app/src/main/java/com/oracto/jitsi/MainActivity.java
@@ -2,9 +2,13 @@ package com.oracto.jitsi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+
+import com.royrodriguez.transitionbutton.TransitionButton;
 
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
@@ -14,10 +18,15 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TransitionButton transitionButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        transitionButton = findViewById(R.id.transition_button);
+
         try {
             JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
                     .setServerURL(new URL(""))
@@ -26,7 +35,44 @@ public class MainActivity extends AppCompatActivity {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
+
+        transitionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the loading animation when the user tap the button
+                transitionButton.startAnimation();
+
+                // Do your networking task or background work here.
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean isSuccessful = true;
+
+                        // Choose a stop animation if your call was succesful or not
+                        if (isSuccessful) {
+                            transitionButton.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
+                                @Override
+                                public void onAnimationStopEnd() {
+
+                                    try {
+                                        startmtn();
+                                    } catch (MalformedURLException e) {
+                                        e.printStackTrace();
+                                        transitionButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
+                                    }
+                                }
+                            });
+                        } else {
+                            transitionButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
+                        }
+                    }
+                }, 2000);
+            }
+        });
     }
+
 
     public void onButtonClick(View v) throws MalformedURLException {
         EditText editText = findViewById(R.id.conferenceName);
@@ -41,6 +87,23 @@ public class MainActivity extends AppCompatActivity {
 
             JitsiMeetActivity.launch(this, options);
         }
+    }
+
+    public void startmtn() throws MalformedURLException {
+
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+        if (text.length() > 0) {
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .setServerURL(new URL("https://vm.farmehr.com"))
+                    .build();
+
+
+            JitsiMeetActivity.launch(this, options);
+        }
+
     }
 
 }
